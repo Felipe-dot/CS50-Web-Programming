82750f3b8f084a4582c55af8607128d97dbf7c91
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-04-06 09:17:43 -0300

    Learning Python basic concepts

---
+++ lecture 2/sequences.py
@@ -0,0 +1,16 @@
+name = "harry"
+
+print(name[0])
+
+# List
+names = ["Harry", "Ron", "Hermione"]
+
+print(names[0])
+
+# Tuples
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0,20.0)
+
+print(coordinate)
